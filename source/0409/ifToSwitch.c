70710377c3124e62e3f736fd111f8a02e73a4e39
june276 <jungjune276@gmail.com>
2024-04-09 14:23:21 +0900

    040901

---
+++ source/0409/ifToSwitch.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+
+int main(){
+
+	int jumsu;
+	char grade;
+
+	scanf("%d", &jumsu);
+
+	switch (jumsu/10)
+	{
+		case 10:
+		case 9:
+			grade = 'A';
+			break;
+		case 8:
+			grade = 'B';
+			break;
+		case 7:
+			grade = 'C';
+			break;
+		default:
+			grade = 'D';
+			break;
+	}
+
+	printf("%c", grade);
+
+	return 0;
+}
