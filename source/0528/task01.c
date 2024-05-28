2c08ccc68343be23c465f99ddba5b48392cdb03b
june276 <jungjune276@gmail.com>
2024-05-28 15:43:13 +0900

    0528

---
+++ source/0528/task01.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main(){
+
+	int a[10] = {10, 20, 30, 40, 50, 60, 79, 80, 90};
+	int *pi;
+	pi = a;
+	printf("pi의 주소 = %d\n", pi);
+	printf("*pi++ = %d\n", *pi++);
+	printf("*pi++ after p의 주소 = %d\n", pi);
+	printf("(*pi)++ = %d\n", (*pi)++);
+	printf("*pi = %d\n", *pi);
+
+	printf("*++pi = %d\n", *++pi);
+	printf("++*p = %d", ++*pi);
+
+	return 0;
+}
